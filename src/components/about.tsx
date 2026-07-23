a3d2919ab67b096cb3afc6cb8ccc6ee9af320ea5
Amrutha km <amruthakm111@gmail.com>
2026-07-23 15:10:30 +0530

    image adding

--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,5 +1,5 @@
 import { FileText } from "lucide-react";
-import profileImg from "../assets/profile.jpg";
+import profileImg from "../assets/profile.JPG";
 
 interface AboutProps {
   resumeMailto: string;
